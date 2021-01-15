977ae1eb61f1e886d0d0a0de5aaf2b84065a892d
Joseph LaMantia Jr <40052124+JosephLaMantia@users.noreply.github.com>
2021-01-15 15:36:23 -0500

    Created App, index.js. Also created necessary components.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Table, TableRow, TableCell } from "./Components"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Test
+    </div>
+  );
+}
+
+export default App;
