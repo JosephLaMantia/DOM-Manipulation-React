ae01366561d70b95db724302d35150f9703e1104
Joseph LaMantia Jr <40052124+JosephLaMantia@users.noreply.github.com>
2021-01-16 18:05:23 -0500

    Add row method created, activated on button click. Added cool matrix styling. closes #2

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,15 @@ export default class App extends Component {
     this.state = {
       numRows: 3,
       numCols: 3,
-      cellColor: "red",
+      cellColor: "",
     };
+  }
+
+  //Add rows to the grid:
+  addRows = () =>{
+    this.setState(
+      {numRows : this.state.numRows + 1}
+    )
   }
 
 
@@ -18,6 +25,14 @@ export default class App extends Component {
     return (
       <div className = "App">
         <h1>Manipulate the grid!</h1>
+
+        <div className = "buttons">
+          <button id="add-rows-btn" onClick={this.addRows}> ADD ROWS </button>
+        </div>
+
+
+
+
         {/* Where grid is stored. */}
         <Table
           numRows = {this.state.numRows}
